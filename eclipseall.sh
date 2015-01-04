c68f8d716969b9fd7e6d479520f78bba0d889367
gisgraphy <davidmasclet@gisgraphy.com>
2015-01-04 14:35:01 +0100

    initial import

---
+++ eclipseall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ../addressParser-commons
+mvn clean eclipse:clean eclipse:eclipse
+cd ../universalserialiser-commons
+mvn clean eclipse:clean eclipse:eclipse
+cd ../universalserialiser
+mvn clean eclipse:clean eclipse:eclipse
+cd ../gisgraphy-commons
+mvn clean eclipse:clean eclipse:eclipse
+cd ../gisgraphy-utils
+mvn clean eclipse:clean eclipse:eclipse
+cd ../gisgraphy
+mvn clean eclipse:clean eclipse:eclipse
---
+++ installall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ../universalserialiser-commons
+mvn clean install -Dmaven.test.skip
+cd ../universalserialiser
+mvn clean install -Dmaven.test.skip
+cd ../gisgraphy-commons
+mvn clean install -Dmaven.test.skip
+cd ../addressParser-commons
+mvn clean install -Dmaven.test.skip
+cd ../gisgraphy-utils
+mvn clean install -Dmaven.test.skip
+if [[ -d "../addressParser-http" ]]
+then
+	cd ../addressParser-http
+	mvn clean install -Dmaven.test.skip
+fi
+
+
